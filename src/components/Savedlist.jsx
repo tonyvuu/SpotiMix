fe5a09490ce406ba93f869bb60ded6315e9625bb
Tony Vu <tony.vuu18@gmail.com>
2023-10-10 11:21:25 -0400

    added saved tracked into save route

--- src/components/Savedlist.jsx
+++ src/components/Savedlist.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Card } from 'react-bootstrap';
 
-function Savedlist() {
+function SavedMusic({savedTrackInfo}) {
   return (
-    <div>Savedlist</div>
-  )
+    <Container>
+      <h2 className="mt-3">Saved Music Tracks</h2>
+      <Row>
+        {savedTrackInfo && savedTrackInfo.length > 0 ? (
+          savedTrackInfo.map((track, index) => (
+            <Card key={index} style={{ width: '18rem' }} className="mt-3">
+              <Card.Img variant="top" src={track.imageUrl} alt={track.name} />
+              <Card.Body>
+                <Card.Title>{track.name}</Card.Title>
+                <Card.Text>Artists: {track.artists}</Card.Text>
+                <Card.Text>Release Date: {track.releaseDate}</Card.Text>
+              </Card.Body>
+            </Card>
+          ))
+        ) : (
+          <p className="mt-3">No saved tracks yet.</p>
+        )}
+      </Row>
+    </Container>
+  );
 }
 
-export default Savedlist
+export default SavedMusic;
