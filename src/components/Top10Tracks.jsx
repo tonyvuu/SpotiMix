9d2de84700870d842fb8efe13f66cd55f682d40e
Tony Vu <tony.vuu18@gmail.com>
2023-10-12 15:37:04 -0400

    added error message

--- src/components/Top10Tracks.jsx
+++ src/components/Top10Tracks.jsx
@@ -1,26 +1,34 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Container, InputGroup, FormControl, Button, Row, Card } from 'react-bootstrap';
-import { FaMoon, FaSun } from 'react-icons/fa'; 
+import { FaMoon, FaSun } from 'react-icons/fa';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import fetchAll from './FetchAPI';  
-import { ThemeContext } from '../App';  
+import fetchAll from './FetchAPI';
+import { ThemeContext } from '../App';
 
-
-function topTracks() {
+function TopTracks({ updateTopTracks }) {
   const [searchInput, setSearchInput] = useState("");
   const [accessToken, setAccessToken] = useState("");
   const [topTracks, setTopTracks] = useState([]);
   const { isDarkMode, toggleTheme } = useContext(ThemeContext);
+  const [savedTracks, setSavedTracks] = useState([]);
+
+  const [errorMessage, setErrorMessage] = useState({
+    index: null,
+    message: ''
+  });
+
+  const [successMessage, setSuccessMessage] = useState({
+    index: null,
+    message: ''
+  });
 
   document.body.style.backgroundColor = isDarkMode ? '#000' : '#fff';
 
-
-  
   useEffect(() => {
     const fetchData = async () => {
-        const token = await fetchAll();  // call fetchAll to get the access token
-        setAccessToken(token);
-      };
+      const token = await fetchAll();  // call fetchAll to get the access token
+      setAccessToken(token);
+    };
 
     fetchData();  // call the fetchData function to fetch the access token
   }, []);
@@ -34,57 +42,88 @@ function topTracks() {
       }
     };
 
-    // fetch artist ID
-      const artistResponse = await fetch(`https://api.spotify.com/v1/search?q=${searchInput}&type=artist`, searchArtistParameters);
-      const artistData = await artistResponse.json();
-      const artistID = artistData.artists.items[0].id;
-    
-    // fetch top tracks for the artist
-      const artistTopTracksResponse = await fetch(`https://api.spotify.com/v1/artists/${artistID}/top-tracks?country=US`, searchArtistParameters);
-      const artistTopTracksData = await artistTopTracksResponse.json();
-      const artistTopTracks = artistTopTracksData.tracks;
-    
-      setTopTracks(artistTopTracks);
-  }
+    const artistResponse = await fetch(`https://api.spotify.com/v1/search?q=${searchInput}&type=artist`, searchArtistParameters);
+    const artistData = await artistResponse.json();
+    const artistID = artistData.artists.items[0].id;
+
+    const artistTopTracksResponse = await fetch(`https://api.spotify.com/v1/artists/${artistID}/top-tracks?country=US`, searchArtistParameters);
+    const artistTopTracksData = await artistTopTracksResponse.json();
+    const artistTopTracks = artistTopTracksData.tracks;
+
+    setTopTracks(artistTopTracks);
+  };
+
   const themeSwitchText = isDarkMode ? 'Dark' : 'Light';
   const themeSwitchIcon = isDarkMode ? <FaMoon color="#fff" /> : <FaSun color="#000" />;
-  
+
+  const handleSaveTrack = (track, index) => {
+    const isTrackSaved = savedTracks.some(savedTrack => savedTrack.name === track.name);
+    if (isTrackSaved) {
+      setErrorMessage({
+        index,
+        message: 'Track is already saved.'
+      });
+      return;
+    }
+
+    const topTrackInfo = {
+      name: track.name,
+      artists: track.artists.map(artist => artist.name).join(', '),
+      releaseDate: track.album.release_date,
+      imageUrl: track.album.images[0].url,
+    };
+
+    updateTopTracks(topTrackInfo);
+    setSavedTracks([...savedTracks, topTrackInfo]);
+
+    setSuccessMessage({
+      index,
+      message: 'Track saved successfully!'
+    });
+  };
+
   return (
     <div>
-       <div className="theme-switch-container">
+      <div className="theme-switch-container">
         <span className="theme-switch-icon" onClick={toggleTheme}>{themeSwitchIcon}</span>
         <span className={`theme-switch-text ${isDarkMode ? 'text-white' : 'text-dark'}`} onClick={toggleTheme}>
-            {themeSwitchText}
-          </span>
+          {themeSwitchText}
+        </span>
       </div>
       <Container>
-      <InputGroup size='lg' className={`m-3`}>
-          <FormControl 
+        <InputGroup size='lg' className={`m-3`}>
+          <FormControl
             placeholder='Search for top tracks of an artist'
             type="input"
-            className={`form-control-${isDarkMode ? 'dark' : 'light'}`} 
+            className={`form-control-${isDarkMode ? 'dark' : 'light'}`}
             onKeyPress={event => {
-              if(event.key === "Enter") {
+              if (event.key === "Enter") {
                 searchAny();
               }
             }}
             onChange={e => setSearchInput(e.target.value)}
           />
-          <Button className='search-bar' onClick={searchAny}>Search 10 Top Ten</Button>        
+          <Button className='search-bar' onClick={searchAny}>Search 10 Top Ten</Button>
         </InputGroup>
       </Container>
       <Container>
         <Row className='mx-2 row row-cols-4'>
           {topTracks.map((track, index) => (
-        <Card className={`tracks-container ${isDarkMode ? 'dark' : 'light'}`} key={index}>
-        <Card.Img src={track.album.images[0].url} alt={`${track.name} album cover`} />
+            <Card className={`tracks-container ${isDarkMode ? 'dark' : 'light'}`} key={index}>
+              <Card.Img src={track.album.images[0].url} alt={`${track.name} album cover`} />
               <Card.Body>
                 <Card.Title>{track.name}</Card.Title>
                 <p>Artist: {track.artists[0].name}</p>
                 <p>Album: {track.album.name}</p>
                 <p>Release Date: {track.album.release_date}</p>
-
+                {successMessage.index === index && (
+                  <div className='success-message'>{successMessage.message}</div>
+                  )}
+                  {errorMessage.index === index && (
+                    <div className='error-message'>{errorMessage.message}</div>
+                  )}
               </Card.Body>
+              <Button className='save-track-button' onClick={() => handleSaveTrack(track, index)}>Save Track</Button>
             </Card>
           ))}
         </Row>
@@ -93,4 +132,4 @@ function topTracks() {
   );
 }
 
-export default topTracks;
+export default TopTracks;
--- src/components/TopAlbums.jsx
+++ src/components/TopAlbums.jsx
@@ -5,118 +5,130 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import fetchAll from './FetchAPI';
 import { ThemeContext } from '../App';  
 
-
-
-function artistAlbum({updateAlbum}) {
+function ArtistAlbum({ updateAlbum }) {
   const [searchInput, setSearchInput] = useState("");
   const [accessToken, setAccessToken] = useState("");
   const [albums, setAlbums] = useState([]);
   const { isDarkMode, toggleTheme } = useContext(ThemeContext);
   const [savedAlbums, setSavedAlbums] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
 
-
-  
   document.body.style.backgroundColor = isDarkMode ? '#000' : '#fff';
-  
-
 
   useEffect(() => {
     const fetchData = async () => {
-        const token = await fetchAll();  // call fetchAll to get the access token
-        setAccessToken(token);
-      };
+      const token = await fetchAll();  // call fetchAll to get the access token
+      setAccessToken(token);
+    };
 
     fetchData();  // call the fetchData function to fetch the access token
   }, []);
-  
 
-// SEARCH
-const searchAny = async () => {
-  console.log(searchInput); // this is the user inputting in the search bar
-
-
-  const searchArtistParameters = {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + accessToken
+  const searchAny = async () => {
+    const searchArtistParameters = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + accessToken
+      }
     }
-  }
-    // fetch artist ID
+
     const artistResponse = await fetch(`https://api.spotify.com/v1/search?q=${searchInput}&type=artist`, searchArtistParameters);
     const artistData = await artistResponse.json();
     const artistID = artistData.artists.items[0].id;
-    console.log("artist id is " + artistID);
-  
-    // fetch albums for the artist
+
     const albumsResponse = await fetch(`https://api.spotify.com/v1/artists/${artistID}/albums?include_groups=album&market=US&limit=50`, searchArtistParameters);
     const albumsData = await albumsResponse.json();
-    console.log(albumsData);
     setAlbums(albumsData.items);
-}
+  }
 
-const themeSwitchText = isDarkMode ? 'Dark' : 'Light';
-const themeSwitchIcon = isDarkMode ? <FaMoon color="#fff" /> : <FaSun color="#000" />;
+  const themeSwitchText = isDarkMode ? 'Dark' : 'Light';
+  const themeSwitchIcon = isDarkMode ? <FaMoon color="#fff" /> : <FaSun color="#000" />;
 
-const handleSaveAlbum = (album) => {
-  // create an album info object
-  const albumInfo = {
-    name: album.name,
-    artist: album.artists[0].name,
-    releaseDate: album.release_date,
-    imageUrl: album.images[0].url,
+  const handleSaveAlbum = (album) => {
+    const isAlbumSaved = savedAlbums.some(savedAlbum => savedAlbum.name === album.name);
+  
+    if (isAlbumSaved) {
+      console.log('Album is already saved:', album.name);
+      // display a message indicating that the album is already saved
+      setErrorMessage({ id: album.id, message: 'Album is already saved.' });
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 2000); // clear the error message after 2 seconds
+      return;
+    }
+
+    // create an album info object
+    const albumInfo = {
+      id: album.id,
+      name: album.name,
+      artist: album.artists[0].name,
+      releaseDate: album.release_date,
+      imageUrl: album.images[0].url,
+    };
+    updateAlbum(albumInfo);
+    setSavedAlbums([...savedAlbums, albumInfo]);
   };
-  updateAlbum(albumInfo)
-  setSavedAlbums([...savedAlbums, albumInfo]);
-};
-
 
   return (
     <div>
-       <div className="theme-switch-container">
+      <div className="theme-switch-container">
         <span className="theme-switch-icon" onClick={toggleTheme}>{themeSwitchIcon}</span>
         <span className={`theme-switch-text ${isDarkMode ? 'text-white' : 'text-dark'}`} onClick={toggleTheme}>
-            {themeSwitchText}
-          </span>
+          {themeSwitchText}
+        </span>
       </div>
-    <Container>
-    <InputGroup size='lg' className={`m-3`}>
-        <FormControl 
-        placeholder='Search for albums of an artist'
-        type = "input"
-        className={`form-control-${isDarkMode ? 'dark' : 'light'}`} 
-        onKeyPress={event => {
-          if(event.key == "Enter") {
-            searchAny()
-          }
-        }}
-        onChange={e => setSearchInput(e.target.value)}
-        />
-      <Button className='search-bar' onClick={searchAny}>Search Albums</Button>        
-      </InputGroup>
-    </Container>
-    <Container>
-  <Row className='mx-2 row row-cols-4'>
-    {albums.map((user, index) => {
-      return (
-        <Card className={`tracks-container ${isDarkMode ? 'dark' : 'light'}`} key={index}>
-        <Card.Img src={user.images[0].url} />
-          <Card.Body>
-            <Card.Title>{user.name}</Card.Title>
-            <p>Artist: {user.artists[0].name}</p>
-            <p>Total Tracks: {user.total_tracks}</p>
-            <p>Release Date: {user.release_date}</p>
-            {/* <p>Type: {user.type}</p> */}
+      <Container>
+        <InputGroup size='lg' className={`m-3`}>
+          <FormControl 
+            placeholder='Search for albums of an artist'
+            type="input"
+            className={`form-control-${isDarkMode ? 'dark' : 'light'}`} 
+            onKeyPress={event => {
+              if(event.key === "Enter") {
+                searchAny()
+              }
+            }}
+            onChange={e => setSearchInput(e.target.value)}
+          />
+          <Button className='search-bar' onClick={searchAny}>Search Albums</Button>        
+        </InputGroup>
+      </Container>
+      <Container>
+        <Row className='mx-2 row row-cols-4'>
+          {albums.map((user, index) => {
+            const isAlbumSaved = savedAlbums.some(savedAlbum => savedAlbum.name === user.name);
+          
+            return (
+              <Card className={`tracks-container ${isDarkMode ? 'dark' : 'light'}`} key={index}>
+                <Card.Img src={user.images[0].url} />
+                <Card.Body>
+                  <Card.Title>{user.name}</Card.Title>
+                  <p>Artist: {user.artists[0].name}</p>
+                  <p>Total Tracks: {user.total_tracks}</p>
+                  <p>Release Date: {user.release_date}</p>
+                </Card.Body>
 
-          </Card.Body>
-          <Button className='save-album-button' onClick={() => handleSaveAlbum(user)}>Save Album</Button>
-        </Card>
-      );
-    })}
-  </Row>
-</Container>
+                <div className="action-buttons">
 
+                  {isAlbumSaved && (
+                    <div className='success-message'>Saved successfully!</div>
+                  )}
+
+                  {errorMessage && errorMessage.id === user.id && (
+                    <div className='error-message'>{errorMessage.message}</div>
+                  )}
+                </div>
+                  <Button className='save-track-button' onClick={() => handleSaveAlbum(user)}>
+                    Save Album
+                  </Button>
+              </Card>
+            );
+          })}
+        </Row>
+      </Container>
     </div>
-  )}
+  );
+}
 
-export default artistAlbum
+export default ArtistAlbum;
